105225bbab86f416d68cff433988e87f496ac505
yasin303 <myasinabdulhafiidh@gmail.com>
2022-08-18 22:24:17 +0700

    admin fiture

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,6 +1,76 @@
+const { Appointment, User, Vaccine } = require('../models')
 class AdminController {
   //Code admin features here
-
+  static appointment(req,res){
+    Appointment.findAll({
+      include: [User, Vaccine]
+    })
+    .then(data => {
+        res.render('home',{data})
+    })
+    .catch(err => {
+        res.send(err)
+    })
+  }
+  static vaccine(req, res){
+    Vaccine.findAll()
+    .then(data =>{
+      res.render('allVaccine', {data})
+    })
+    .catch(err=>{
+      res.render(err)
+    })
+  }
+  static add(req, res){
+    res.render('formAddVaccine')
+  }
+  static addpost(req, res){
+    const { name, stock, category } = req.body
+    Vaccine.create({name, stock, category})
+    .then(_=>{
+      res.redirect('/admins/vaccine')
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  }
+  static edit(req, res){
+    const id = req.params.id
+    Vaccine.findOne({
+      where: {id}
+    })
+    .then(data=>{
+      res.render('editVaccine', {data})
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  }
+  static editpost(req, res){
+    const { name, stock, category } = req.body
+    const id = req.params.id
+    Vaccine.update({name, stock, category},{
+      where: {id}
+    })
+    .then(_=>{
+      res.redirect('/admins/vaccine')
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  }
+  static delete(req,res){
+    const id = req.params.id
+    Vaccine.update({isDeleted:1},{
+      where: {id}
+    })
+    .then(_=>{
+      res.redirect('/admins/vaccine')
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  }
 }
 
 module.exports = AdminController;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -7,17 +7,24 @@ router.get('/login', AuthController.formLoginAdmin);
 router.post('/login', AuthController.loginAdmin);
 
 //Middleware (only allows user with role admin to access
-router.use((req, res, next) => {
-  const {adminId, role} = req.session;
-  if (!adminId && !role || role !== 'Admin') {
-    res.send('404 Not Found');
-    return;
-  }
-  next();
-});
+// router.use((req, res, next) => {
+//   const {adminId, role} = req.session;
+//   if (!adminId && !role || role !== 'Admin') {
+//     res.send('404 Not Found');
+//     return;
+//   }
+//   next();
+// });
 
 //Admin logout
+router.get('/', AdminController.appointment)
 router.get('/logout', AuthController.logout);
+router.get('/vaccine', AdminController.vaccine)
+router.get('/vaccine/add', AdminController.add)
+router.post('/vaccine/add', AdminController.addpost)
+router.get('/vaccine/edit/:id', AdminController.edit)
+router.post('/vaccine/edit/:id', AdminController.editpost)
+router.get('/vaccine/delete/:id', AdminController.delete)
 
 router.get('/', (req, res) => {
   res.send('Hello Admin');
